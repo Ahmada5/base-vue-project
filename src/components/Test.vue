58a30becee5b14379835d57b72537bba40a703f2
ahmada <achmad.ahmada@gmail.com>
2024-08-20 11:59:25 +0700

    upld-base

---
+++ src/components/Test.vue
@@ -0,0 +1,12 @@
+<template>
+    <p>testing</p>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-facing-decorator'
+
+@Component
+export default class Test extends Vue {
+
+}
+</script>
